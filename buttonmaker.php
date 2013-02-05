1919a719bcb05c1abaca109e98e51d2f942be989
Andy Quigley <ajquigley@yahoo.com>
2013-02-05 12:00:46 -0500

    Copyright notice and LGPL license added for publication on github

--- buttonmaker.php
+++ buttonmaker.php
@@ -6,6 +6,25 @@ Description: Creates a Shortcode of 'button' which encloses some text to be disp
 Version: 0.1
 Author: Andy Quigley
 */
+/* 
+buttonmaker - a Wordpress plugin which adds a shortcode of [button] to enclose text and an optional link
+Copyright (C) 2010-2013 Andy Quigley <ajquigley@yahoo.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the
+Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+Boston, MA  02110-1301, USA.  
+*/
 function buttonmaker_button($attr, $content=''){
     if ($content == '') return standardspop_popup();
     $graphic = $attr[graphic];
